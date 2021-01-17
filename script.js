0086611bcf551413e9821c2002fdd4de3bb13fae
Emma <eschaale@u.rochester.edu>
2021-01-17 14:54:16 -0500

    All features working

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonClose = document.querySelector('.close-modal');
+const buttonOpen = document.querySelectorAll('.show-modal');
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+const openModal = function() {
+    
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    
+};
+
+for (let i = 0; i < buttonOpen.length; i++) {
+    buttonOpen[i].addEventListener('click', openModal);
+};
+
+//Close the modal
+buttonClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "Escape" && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
